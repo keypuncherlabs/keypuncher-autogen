e2ac06e747deb515c47238c65f3256fe8d93e4f6
cmarkovich <cmarkovich@keypuncher.com>
2023-12-02 15:40:48 -0600

    Adding argument as optional cli command to execute specified prompt file

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+import argparse
 from agents.assistant_agent import AssistantAgent
 from agents.user_proxy_agent import UserProxyAgent
 from utils.utils import Utils
 
 
 def main():
+    parser = argparse.ArgumentParser(
+        description='Run Autogen with a specified prompt file.')
+    parser.add_argument('prompt_file', nargs='?', default='generate_random_numbers.txt',
+                        help='Prompt file name (default: generate_random_numbers.txt)')
+    args = parser.parse_args()
+
     assistant_agent = AssistantAgent()
     user_proxy_agent = UserProxyAgent()
 
     # Create the prompt using the utility function
-    prompt = Utils.create_prompt('fetch_weather.txt')
+    prompt = Utils.create_prompt(args.prompt_file)
 
     if prompt:
         # Initiate chat with Autogen
@@ -16,7 +23,7 @@ def main():
             assistant_agent.assistant, message=prompt)
 
         # Handle the chat response
-        # Note: You'll need to implement a loop or some logic to handle ongoing interaction
+        # Note: Implement a loop or logic to handle ongoing interaction
         # depending on how your Autogen system is set up.
         # print("Autogen Response: ", response)
 
