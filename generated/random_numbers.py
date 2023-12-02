9a41fb3811c7377effd95b1dc86cd163a03d2712
cmarkovich <cmarkovich@keypuncher.com>
2023-12-02 11:19:27 -0600

    Appending more contextual instructions to base prompt

---
+++ generated/random_numbers.py
@@ -0,0 +1,6 @@
+# filename: random_numbers.py
+import random
+
+# Generate and print 100 random whole numbers between 1 and 1000
+for _ in range(100):
+    print(random.randint(1, 1000))
--- main.py
+++ main.py
@@ -6,13 +6,20 @@ def main():
     assistant_agent = AssistantAgent()
     user_proxy_agent = UserProxyAgent()
 
-    # Read the prompt
+    # Read the generate numbers prompt
     with open('prompts/generate_random_numbers.txt', 'r') as file:
         generate_numbers_prompt = file.read()
 
+    # Read the general instructions
+    with open('prompts/general_instructions.txt', 'r') as file:
+        general_instructions = file.read()
+
+    # Combine both prompts
+    combined_prompt = generate_numbers_prompt + "\n" + general_instructions
+
     # Initiate chat with Autogen
     user_proxy_agent.user_proxy.initiate_chat(
-        assistant_agent.assistant, message=generate_numbers_prompt)
+        assistant_agent.assistant, message=combined_prompt)
 
     # Handle the chat response
     # Note: This is a simplified example. You'll need to implement a loop or some logic
